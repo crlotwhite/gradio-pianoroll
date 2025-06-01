1aa7d9e6aa8a8797afa35651a03ee2a4a8dfebca
crlotwhite <crlotwhite@gmail.com>
2025-06-01 23:08:00 +0900

    Add svelte-pianoroll

--- frontend/Index.svelte
+++ frontend/Index.svelte
@@ -7,6 +7,8 @@
 	import type { LoadingStatus } from "@gradio/statustracker";
 	import type { SelectData } from "@gradio/utils";
 
+	import PianoRoll from "./components/PianoRoll.svelte";
+
 	export let elem_id = "";
 	export let elem_classes: string[] = [];
 	export let visible = true;
@@ -21,6 +23,34 @@
 		input: never;
 		clear_status: LoadingStatus;
 	}>;
+
+	// Sample notes data
+	const sampleNotes = [
+    {
+      id: 'note-1',
+      start: 80, // 1st beat of measure 1
+      duration: 80, // Quarter note
+      pitch: 60, // Middle C
+      velocity: 100,
+      lyric: '안녕'
+    },
+    {
+      id: 'note-2',
+      start: 160, // 1st beat of measure 2
+      duration: 160, // Half note
+      pitch: 64, // E
+      velocity: 90,
+      lyric: '하세요'
+    },
+    {
+      id: 'note-3',
+      start: 320, // 1st beat of measure 3
+      duration: 80, // Quarter note
+      pitch: 67, // G
+      velocity: 95,
+      lyric: '반가워요'
+    }
+  ];
 </script>
 
 <Block {visible} {elem_id} {elem_classes} {container} {scale} {min_width}>
@@ -34,4 +64,5 @@
 	{/if}
 
 	<JsonView json={value} />
+	<PianoRoll width={1000} height={600} notes={sampleNotes} />
 </Block>
